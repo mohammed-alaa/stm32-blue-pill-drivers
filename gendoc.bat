e9a9a060c2fa21d1741369dcbabf33c23aa44532
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-06-17 11:54:27 +0300

    Updated `gendoc` batch to move the PDF to the correct path

--- gendoc.bat
+++ gendoc.bat
@@ -2,14 +2,11 @@
 ECHO "Deleting old documentation..."
 @RD /S /Q ".\docs\html"
 @RD /S /Q ".\docs\latex"
-@RD /S /Q ".\local_docs"
 ECHO "Generating new documentation..."
 doxygen Doxyfile
 SETLOCAL
 CALL .\docs\latex\make.bat
 ENDLOCAL
-MKDIR ".\local_docs\html"
-MOVE /Y ".\docs\html" ".\local_docs\html
-MKDIR ".\local_docs\pdf"
-COPY /Y ".\docs\latex\refman.pdf" ".\local_docs\pdf\documentation.pdf"
+MKDIR ".\docs\html\docs\pdf"
+COPY /Y ".\docs\latex\refman.pdf" ".\docs\html\docs\pdf\documentation.pdf"
 @RD /S /Q ".\docs\latex"
