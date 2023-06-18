59904a7ce4b38db6e318c462a1043aaa613a4b83
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-06-18 16:27:14 +0300

    Added standard values

---
+++ src/COTS/LIB/LSTD_VALUES.h
@@ -0,0 +1,47 @@
+/**
+ * @file LSTD_VALUES.h
+ * @author Mohamed Alaa
+ * @brief This file contains the standard values
+ * @version 1.0.0
+ * @date 2023-06-18
+ */
+
+#ifndef _LSTD_VALUES_H_
+#define _LSTD_VALUES_H_
+
+#include "LSTD_TYPES.h"
+#include "LSTD_COMPILER.h"
+
+/**
+ * @addtogroup standard_library
+ * @{
+ */
+
+/**
+ * @defgroup standardvalues Standard values
+ * @brief This module contains the standard values
+ * @{
+ */
+
+/**
+ * @def TRUE
+ * @brief Type definition for TRUE
+ */
+#define TRUE ((t_bool)1)
+
+/**
+ * @def FALSE
+ * @brief Type definition for FALSE
+ */
+#define FALSE ((t_bool)0)
+
+/**
+ * @def NULL
+ * @brief Type definition for NULL
+ */
+#define NULL ((P2VAR(void))0)
+
+/** @} */
+/** @} */
+
+#endif /* _LSTD_VALUES_H_ */
