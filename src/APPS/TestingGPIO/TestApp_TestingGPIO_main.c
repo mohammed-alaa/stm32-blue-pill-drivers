98eb720fb18623e785a6755ad762367bb2a70b96
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-06-26 01:08:25 +0300

    Added `GPIO` initial testing application

---
+++ src/APPS/TestingGPIO/TestApp_TestingGPIO_main.c
@@ -0,0 +1,20 @@
+/**
+ * @file TestApp_TestingGPIO_main.c
+ * @author Mohamed Alaa
+ * @version 1.0.0
+ * @brief This file contains the main implementation for the TestApp_TestingGPIO application.
+ * @details This file contains the main implementation for the TestApp_TestingGPIO application.
+ * @date 2023-06-25
+ */
+
+#include "TestApp_TestingGPIO_main.h"
+#include "../../COTS/MCAL/GPIO/GPIO_interface.h"
+
+void vTestApp_TestingGPIO_main(void)
+{
+
+	for (;;)
+	{
+		/* Do nothing */
+	}
+}
---
+++ src/APPS/TestingGPIO/TestApp_TestingGPIO_main.h
@@ -0,0 +1,15 @@
+/**
+ * @file TestApp_TestingGPIO_main.h
+ * @author Mohamed Alaa
+ * @version 1.0.0
+ * @brief This file contains the main header file for the TestApp_TestingGPIO application.
+ * @details This file contains the main header file for the TestApp_TestingGPIO application.
+ * @date 2023-06-25
+ */
+
+#ifndef _TestApp_TestingGPIO_MAIN_H_
+#define _TestApp_TestingGPIO_MAIN_H_
+
+void vTestApp_TestingGPIO_main(void);
+
+#endif /* _TestApp_TestingGPIO_MAIN_H_ */
