4697f0974659e2f9518985e7d80f5c485af98a0b
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-06-26 23:17:26 +0300

    Updated documentation

--- src/APPS/TestingGPIO/TestApp_TestingGPIO_main.h
+++ src/APPS/TestingGPIO/TestApp_TestingGPIO_main.h
@@ -7,9 +7,26 @@
  * @date 2023-06-25
  */
 
+/**
+ * @dir TestingGPIO
+ * @brief This directory contains the testing application for the GPIO driver
+ * @details This directory contains the testing application for the GPIO driver
+ */
+
 #ifndef _TestApp_TestingGPIO_MAIN_H_
 #define _TestApp_TestingGPIO_MAIN_H_
 
+/**
+ * @addtogroup testing_applications
+ * @{
+ */
+
+/**
+ * @brief This function is the main function for the TestApp_TestingGPIO application
+ * @details This function is the main function for the TestApp_TestingGPIO application
+ */
 void vTestApp_TestingGPIO_main(void);
+
+/** @} */
 
 #endif /* _TestApp_TestingGPIO_MAIN_H_ */
