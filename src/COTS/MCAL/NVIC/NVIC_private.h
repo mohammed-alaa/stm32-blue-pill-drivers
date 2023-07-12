1ec3f9d09bcfddb93f6c9a78c4c836a703e160ef
Mohamed Alaa <30788249+mohammed-alaa@users.noreply.github.com>
2023-07-12 04:22:41 +0300

    Added `NVIC` module inital files

---
+++ src/COTS/MCAL/NVIC/NVIC_private.h
@@ -0,0 +1,13 @@
+/**
+ * @file NVIC_private.h
+ * @author Mohamed Alaa
+ * @version 1.0.0
+ * @brief This file contains the private macros and registers for the NVIC module.
+ * @details This file contains the private macros and registers for the NVIC module.
+ * @date 2023-07-12
+ */
+
+#ifndef _NVIC_PRIVATE_H_
+#define _NVIC_PRIVATE_H_
+
+#endif /* _NVIC_PRIVATE_H_ */
